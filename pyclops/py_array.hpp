a22d19bb2e30cf3a8d99faed277c40614fde847a
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-08-02 10:10:30 -0400

    py_array::flags() + comments

--- pyclops/py_array.hpp
+++ pyclops/py_array.hpp
@@ -19,12 +19,42 @@ struct py_array : public py_object {
 
     inline PyArrayObject *aptr() const { return reinterpret_cast<PyArrayObject *> (this->ptr); }
 
-    int ndim() const           { return PyArray_NDIM(aptr()); }
-    npy_intp *shape() const    { return PyArray_SHAPE(aptr()); }
-    npy_intp *strides() const  { return PyArray_STRIDES(aptr()); }
-    npy_intp itemsize() const  { return PyArray_ITEMSIZE(aptr()); }
-    void *data() const         { return PyArray_DATA(aptr()); }
-    int npy_type() const       { return PyArray_TYPE(aptr()); }
+    inline int ndim() const           { return PyArray_NDIM(aptr()); }
+    inline npy_intp *shape() const    { return PyArray_SHAPE(aptr()); }
+    inline npy_intp *strides() const  { return PyArray_STRIDES(aptr()); }
+    inline npy_intp itemsize() const  { return PyArray_ITEMSIZE(aptr()); }
+    inline void *data() const         { return PyArray_DATA(aptr()); }
+    inline int npy_type() const       { return PyArray_TYPE(aptr()); }
+
+    // Reminder: the numpy flags are as follows:
+    //
+    //   NPY_ARRAY_C_CONTIGUOUS
+    //   NPY_ARRAY_F_CONTIGUOUS
+    //   NPY_ARRAY_OWNDATA           free 'data' pointer when array object is deallocated
+    //   NPY_ARRAY_ALIGNED           aligned in multiples of sizeof(T)
+    //   NPY_ARRAY_NOTSWAPPED        data has native endianness
+    //   NPY_ARRAY_UPDATEIFCOPY      write to base->data when array object is deallocated
+    //   NPY_ARRAY_WRITEABLE
+    //
+    // The following flags aren't stored in the PyArray_FLAGS, but can be specified in
+    // constructor functions such as PyArray_New().
+    //
+    //   NPY_ARRAY_FORCECAST         cause a cast to occur regardless of whether or not it is safe
+    //   NPY_ARRAY_ENSURECOPY        always copy the array (returned array is contiguous and writeable)
+    //   NPY_ARRAY_ENSUREARRAY       make sure the returned array is a base-class ndarray
+    //   NPY_ARRAY_ELEMENTSTRIDES    make sure that the strides are in units of the element size
+
+    inline int flags() const { return PyArray_FLAGS(aptr()); }
+
+    // py_array::_base() wraps PyArray_BASE(), but this can return NULL if the array owns its
+    // own memory.  In this case, we just return a reference to the array.
+    // 
+    // From the numpy documentation: 
+    // 
+    //   In most cases, 'base' is the object which owns the memory the array is pointing at.  
+    //   If the NPY_ARRAY_UPDATEIFCOPY flag is set, it has a different  meaning, namely 'base'
+    //   is the array into which the current array will be copied upon destruction. 
+    //   This overloading of 'base' is likely to change in a future version of NumPy. (!!)
 
     py_object _base() const
     {

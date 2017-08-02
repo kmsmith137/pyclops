539eca81a3afc0f73b8feaa8eebbe9a307fb43a5
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-08-02 18:34:08 -0400

    cfunction_table cleanups

--- cfunction_table.cpp
+++ cfunction_table.cpp
@@ -9,25 +9,33 @@ namespace pyclops {
 #endif
 
 
-static constexpr int table_capacity = 50;
+static constexpr int max_kwargs_cfunctions = 50;
 
 
-struct kwargs_entry {
-    PyCFunction c_func;
+// -------------------------------------------------------------------------------------------------
+//
+// kwargs_cfunction
+//
+// Input:    std::function<py_object(py_tuple,py_dict)>
+// Output:   PyObject (*)(PyObject *, PyObject *, PyObject *)
+
+
+struct kwargs_cfunction {
     std::function<py_object(py_tuple,py_dict)> cpp_func;
+    PyObject * (*c_func)(PyObject *, PyObject *, PyObject *);
 };
 
-static vector<kwargs_entry> kwargs_table(table_capacity);
-static int kwargs_table_size = 0;
+static vector<kwargs_cfunction> kwargs_cfunctions(max_kwargs_cfunctions);
+static int num_kwargs_cfunctions = 0;
 
 
 // non-inline
-PyObject *_f_kwargs(PyObject *self, PyObject *args, PyObject *kwds, int N)
+PyObject *_kwargs_cfunction_body(PyObject *self, PyObject *args, PyObject *kwds, int N)
 {
     try {
 	py_tuple a = py_tuple::borrowed_reference(args);
 	py_dict k = kwds ? py_dict::borrowed_reference(kwds) : py_dict();
-	py_object r = kwargs_table[N].cpp_func(a, k);
+	py_object r = kwargs_cfunctions[N].cpp_func(a, k);
 
 	PyObject *ret = r.ptr;
 	r.ptr = NULL;  // steal reference
@@ -43,40 +51,41 @@ PyObject *_f_kwargs(PyObject *self, PyObject *args, PyObject *kwds, int N)
 }
 
 template<int N>
-static PyObject *f_kwargs(PyObject *self, PyObject *args, PyObject *kwds)
+static PyObject *kwargs_cfunction_body(PyObject *self, PyObject *args, PyObject *kwds)
 {
-    return _f_kwargs(self, args, kwds, N);
+    return _kwargs_cfunction_body(self, args, kwds, N);
 }
 
 
 template<int N, typename std::enable_if<(N==0),int>::type = 0>
-void populate_kwargs_table() { }
+void initialize_kwargs_cfunctions() { }
 
 template<int N, typename std::enable_if<(N>0),int>::type = 0>
-void populate_kwargs_table()
+void initialize_kwargs_cfunctions()
 {
-    populate_kwargs_table<N-1>();
-    
-    PyObject * (*f)(PyObject *, PyObject *, PyObject *) = f_kwargs<N-1>;
-    kwargs_table[N-1].c_func = (PyCFunction) f;
+    initialize_kwargs_cfunctions<N-1>();    
+    kwargs_cfunctions[N-1].c_func = kwargs_cfunction_body<N-1>;
 }
 
 
 PyCFunction make_kwargs_cfunction(std::function<py_object(py_tuple,py_dict)> f)
 {
-    if (kwargs_table_size >= table_capacity)
+    if (num_kwargs_cfunctions >= max_kwargs_cfunctions)
 	throw runtime_error("pyclops: cfunction_table is full!");
 
-    kwargs_table[kwargs_table_size].cpp_func = f;
-    return kwargs_table[kwargs_table_size++].c_func;
+    kwargs_cfunctions[num_kwargs_cfunctions].cpp_func = f;
+    return (PyCFunction) kwargs_cfunctions[num_kwargs_cfunctions++].c_func;
 }
+
+
+// -------------------------------------------------------------------------------------------------
 
 
 namespace {
     struct _initializer {
 	_initializer() 
 	{
-	    populate_kwargs_table<table_capacity> ();
+	    initialize_kwargs_cfunctions<max_kwargs_cfunctions> ();
 	}
     } _ini;
 }

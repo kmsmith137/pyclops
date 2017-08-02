8d76a1783dd4c79a90c8d1026a29ab5b061a81c7
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-08-02 08:42:37 -0400

    pyclops extension module

---
+++ pyclops.cpp
@@ -0,0 +1,27 @@
+// This source file is compiled into pyclops.so (the python extension module).
+// It is not compiled into libpyclops.so (the C++ library).
+
+#include "pyclops.hpp"
+
+using namespace std;
+using namespace pyclops;
+
+
+static PyMethodDef module_methods[] = {
+    { NULL, NULL, 0, NULL }
+};
+
+
+PyMODINIT_FUNC initpyclops(void)
+{
+    import_array();
+
+    if (!_reaper_type_ready())
+	return;
+
+    PyObject *m = Py_InitModule3("pyclops", module_methods, "pyclops: a C++ library for writing python extension modules");
+    if (!m)
+        return;
+
+    _add_reaper_type(m);
+}
